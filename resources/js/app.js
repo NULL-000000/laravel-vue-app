1909259a4c35678fc498f433c25d05f40057079d
NULL-000000 <black.design.mini@gmail.com>
2021-11-28 17:53:21 +0900

    try test

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import './bootstrap'
 import Vue from 'vue'
-import ArticleLike from './components/ArticleLike'
 import ArticleTagsInput from './components/ArticleTagsInput'
+import ArticleLike from './components/ArticleLike'
 import FollowButton from './components/FollowButton'
 
 const app = new Vue({
