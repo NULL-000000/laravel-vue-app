384fedd31b9938c51208a7a4a21e2ce314009207
NULL-000000 <black.design.mini@gmail.com>
2021-12-01 07:02:37 +0900

    MySQLのDockerコンテナの修正

--- config/database.php
+++ config/database.php
@@ -93,7 +93,7 @@ return [
 
         'circle_testing' => [
             'driver' => 'mysql',
-            'host' => '127.0.0.1',
+            'host' => 'localhost',
             'port' => '3306',
             'database' => 'larasns',
             'username' => 'default',
