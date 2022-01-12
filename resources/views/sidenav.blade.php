b23a506f47734068a0a5a32fdb68f49dde81be54
NULL-000000 <black.design.mini@gmail.com>
2022-01-12 19:53:02 +0900

    [feature]タグ一覧の実装

--- resources/views/sidenav.blade.php
+++ resources/views/sidenav.blade.php
@@ -1,3 +1,21 @@
+{{-- タグ一覧 --}}
+<div class="col-lg-8 col-md-10 col-sm-12 col-xs-12 mx-auto mt-5">
+    <div class="card mb-4 sidebar-content">
+        <div class="card-header text-center">
+            <i class="fas fa-tags mr-2">
+            </i>メインタグ
+        </div>
+        <div class="card-body main-tag-list py-3 mx-auto">
+            @foreach ($allTagNames as $tag)
+                <a href="{{ route('tags.show', ['name' => $tag->name]) }}">
+                    <p>{{ $tag->hashtag }}</p>
+                </a>
+            @endforeach
+        </div>
+    </div>
+</div>
+{{-- タグ一覧 --}}
+
 {{-- 記事一覧並び替え --}}
 <div class="col-lg-8 col-md-10 col-sm-12 col-xs-12 mx-auto mt-5">
     <h6 class="text-left">
