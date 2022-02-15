f8285e2a3f1a1581ce01e3c0e21b49328045522c
NULL-000000 <black.design.mini@gmail.com>
2022-02-16 06:23:42 +0900

    [fix]達成チェック画面の修正

--- resources/views/achievement/form.blade.php
+++ resources/views/achievement/form.blade.php
@@ -1,20 +1,23 @@
 <li class="achievement-form">
     <div class="description">
-        <form method="POST" action="{{ route('achievement.update', ['article' => $article]) }}">
-            @method('PATCH')
-            @csrf
-            @if ($achievement == 'success')
-                <div class="form-header success">
-                    <div class="form-title">
-                        SENGEN 達成<i class="fas fa-check ml-1"></i>
-                    </div>
+        @if ($achievement == 'success')
+            <div class="form-header success">
+                <div class="form-title">
+                    SENGEN 達成<i class="fas fa-check ml-1"></i>
                 </div>
+            </div>
+            <div class="form-card">
+                @include('articles.detail')
+            </div>
+            <form method="POST" action="{{ route('achievement.update', ['article' => $article]) }}">
+                @method('PATCH')
+                @csrf
                 <div class="form-body">
                     @include('error_card_list')
                     <label>学び・反省</label>
-                    <input type="text" name="study" class="form-control mb-2" value="">
+                    <textarea name="study" required class="form-control mb-2" rows="2" placeholder="本文"></textarea>
                     <label>次回の意気込み</label>
-                    <input type="text" name="enthusiasm" class="form-control" value="">
+                    <textarea name="enthusiasm" required class="form-control mb-2" rows="2" placeholder="本文"></textarea>
                     <input type="hidden" name="achievement" value="success">
                     <input type="hidden" name="cause" value="unspecified">
                     <input type="hidden" name="solution" value="unspecified">
@@ -24,16 +27,24 @@
                         <span class="form-check-text">達成</span>
                     </button>
                 </div>
-            @elseif ($achievement == 'failure')
-                <div class="form-header failure">
-                    <div class="form-title">SENGEN 失敗<i class="fas fa-times ml-1"></i></div>
-                </div>
+            </form>
+
+        @elseif ($achievement == 'failure')
+            <div class="form-header failure">
+                <div class="form-title">SENGEN 失敗<i class="fas fa-times ml-1"></i></div>
+            </div>
+            <div class="form-card">
+                @include('articles.detail')
+            </div>
+            <form method="POST" action="{{ route('achievement.update', ['article' => $article]) }}">
+                @method('PATCH')
+                @csrf
                 <div class="form-body">
                     @include('error_card_list')
                     <label>失敗した原因</label>
-                    <input type="text" name="cause" class="form-control mb-2" value="">
+                    <textarea name="cause" required class="form-control mb-2" rows="2" placeholder="本文"></textarea>
                     <label>次回の対策</label>
-                    <input type="text" name="solution" class="form-control" value="">
+                    <textarea name="solution" required class="form-control mb-2" rows="2" placeholder="本文"></textarea>
                     <input type="hidden" name="achievement" value="failure">
                     <input type="hidden" name="study" value="unspecified">
                     <input type="hidden" name="enthusiasm" value="unspecified">
@@ -43,7 +54,7 @@
                         <span class="form-check-text">失敗</span>
                     </button>
                 </div>
-            @endif
-        </form>
+            </form>
+        @endif
     </div>
 </li>
