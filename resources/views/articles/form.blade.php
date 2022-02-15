5aab6526346ca4755af57dbbebf5f9fd2d960846
NULL-000000 <black.design.mini@gmail.com>
2022-02-16 05:13:19 +0900

    [fix]記事入力フォームのレスポンシブ対応

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -1,55 +1,5 @@
-{{-- @csrf
-<div class="md-form">
-    <label>タイトル</label>
-    <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}">
-</div>
-<div class="form-group">
-    <article-tags-input :initial-tags='@json($tagNames ?? [])' :autocomplete-items='@json($allTagNames ?? [])'>
-    </article-tags-input>
-</div>
-<div class="form-group">
-    <label>日付と時刻</label>
-    <input type="datetime-local" name="period" class="form-control" required
-        value="{{ $article->period ?? old('period') }}">
-</div>
-<div class="form-group">
-    <label></label>
-    <textarea name="body" required class="form-control" rows="16"
-        placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
-</div> --}}
-
-{{-- <div class="form-content">
-    <div class="form-header">
-        <h5 class="modal-title" id="exampleModalLabel">TO DO SENGEN</h5>
-    </div>
-    <div class="form-body">
-        <div class="md-form">
-            <label>タイトル</label>
-            <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}">
-        </div>
-        <div class="form-group">
-            <article-tags-input :initial-tags='@json($tagNames ?? [])' :autocomplete-items='@json($allTagNames ?? [])'>
-            </article-tags-input>
-        </div>
-        <div class="form-group">
-            <label>日付と時刻</label>
-            <input type="datetime-local" name="period" class="form-control" required
-                value="{{ $article->period ?? old('period') }}">
-        </div>
-        <div class="form-group">
-            <label></label>
-            <textarea name="body" required class="form-control" rows="16"
-                placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
-        </div>
-    </div>
-    <div class="form-footer">
-        <button type="submit" class="btn default-color btn-block">投稿する</button>
-    </div>
-</div> --}}
-
 <div class="articles-form">
     <div class="description">
-        {{-- <form method="POST" action="{{ route('articles.store') }}"> --}}
         @csrf
         <div class="form-header">
             <div class="form-title" id="exampleModalLabel">TO DO SENGEN<i class="fas fa-check ml-2"></i></div>
@@ -81,7 +31,7 @@
                 <textarea name="body" required class="form-control" rows="5"
                     placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
             </div>
-            @if (isset($article) && $article->status == "success")
+            @if (isset($article) && $article->status == 'success')
                 <div class="form-group">
                     <label>学び・反省</label>
                     <textarea name="study" required class="form-control" rows="2"
@@ -92,7 +42,7 @@
                     <textarea name="enthusiasm" required class="form-control" rows="2"
                         placeholder="本文">{{ $article->achievement->enthusiasm }}</textarea>
                 </div>
-            @elseif (isset($article) && $article->status === "failure")
+            @elseif (isset($article) && $article->status === 'failure')
                 <div class="form-group">
                     <label>原因</label>
                     <textarea name="cause" required class="form-control" rows="2"
@@ -106,9 +56,6 @@
             @endif
         </div>
         <div class="form-footer m-2">
-            {{-- <button type="submit" class="btn default-color text-white">
-                    SENGEN
-                </button> --}}
             <div class="card-check">
                 <button type="submit" class="check-btn default-color">
                     <span class="check-text">
@@ -117,6 +64,5 @@
                 </button>
             </div>
         </div>
-        {{-- </form> --}}
     </div>
 </div>
