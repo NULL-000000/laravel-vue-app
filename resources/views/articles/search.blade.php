f93ba1c6e1ed45f88df51e0547fa66ec4560fc72
NULL-000000 <black.design.mini@gmail.com>
2022-02-18 06:25:00 +0900

    [fix]検索結果画面の修正

--- resources/views/articles/search.blade.php
+++ resources/views/articles/search.blade.php
@@ -6,13 +6,35 @@
 
     @include('nav')
 
-    <div class="row justify-content-center mt-5">
-        <div class='col-md-7'>
+    {{-- <div class="row justify-content-center mt-5">
+        <div class='col-md-7'> --}}
             <div class="container">
                 <form method="GET" action="{{ route('articles.search') }}">
-                    <div class="card">
+                    <div class="card mt-5">
                         <div class="card-body">
-                            <div class="row">
+                            <ul class="search-card">
+                                <li class="keyword">
+                                    {{-- <label for="keyword">Title</label> --}}
+                                    <input type="text" name="keyword" value="{{ $keyword }}" class="form-control"
+                                        placeholder="検索...">
+                                </li>
+                                <li class="sort">
+                                    {{-- <label for="status">Sort</label> --}}
+                                    <select name="sort" id="sort" class="form-control custom-select">
+                                        @foreach (config('consts.articles.sort_type') as $sort_type)
+                                            <option value={{ $sort_type['sort_value'] }} @if ($sort === $sort_type['sort_value']) selected @endif>
+                                                {{ $sort_type['sort_text'] }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </li>
+                                <li class="submit">
+                                    <input type="hidden" name="status" value={{ $status }} class="form-control">
+                                    {{-- <input type="submit" value="Search" class="btn btn-primary btn-block"> --}}
+                                    <button type="submit"><i class="fas fa-search"></i></button>
+                                </li>
+                            </ul>
+                            {{-- <div class="row">
                                 <div class="form-group col-sm-6">
                                     <label for="keyword">Title</label>
                                     <input type="text" name="keyword" value="{{ $keyword }}" class="form-control"
@@ -32,7 +54,7 @@
                                     <input type="hidden" name="status" value={{ $status }} class="form-control">
                                     <input type="submit" value="Search" class="btn btn-primary btn-block">
                                 </div>
-                            </div>
+                            </div> --}}
                         </div>
                     </div>
                 </form>
@@ -65,8 +87,8 @@
                     @endforeach
                 </div>
             </div>
-        </div>
-    </div>
+        {{-- </div>
+    </div> --}}
 
     @include('footer')
 
