e5249c19838bfcd31c59697d550c52954f6c0940
NULL-000000 <black.design.mini@gmail.com>
2022-02-20 14:52:24 +0900

    [fix]サイドバーの修正

--- resources/views/sidenav.blade.php
+++ resources/views/sidenav.blade.php
@@ -1,15 +1,4 @@
-<div class="ranking"><i class="fas fa-tags mr-2"></i>タグランキング</div>
-<div class="f-container">
-    @foreach ($tags_ranking as $tag)
-        <a href="{{ route('tags.show', ['name' => $tag->name]) }}">
-            <div class="f-item">
-                {{ $tag->hashtag }}
-                <span>{{ $tag->articles_count }}</span>件
-            </div>
-        </a>
-    @endforeach
-</div>
-<div class="ranking"><i class="fas fa-tags mr-2"></i>達成ランキング</div>
+{{-- <div class="ranking"><i class="fas fa-tags mr-2"></i>達成ランキング</div>
 <div class="f-container">
     @foreach ($users_ranking as $user)
         <a href="{{ route('users.show', ['name' => $user->name]) }}">
@@ -20,3 +9,42 @@
         </a>
     @endforeach
 </div>
+<div class="ranking"><i class="fas fa-tags mr-2"></i>タグランキング</div>
+<div class="f-container">
+    @foreach ($tags_ranking as $tag)
+        <a href="{{ route('tags.show', ['name' => $tag->name]) }}">
+            <div class="f-item">
+                {{ $tag->hashtag }}
+                <span>{{ $tag->articles_count }}</span>件
+            </div>
+        </a>
+    @endforeach
+</div> --}}
+
+<div class="ranking"><i class="fas fa-check mr-2"></i>達成ランキング</div>
+<ul class="user_ranking">
+    @foreach ($users_ranking as $user)
+        <div class="ranking-item">
+            <a href="{{ route('users.show', ['name' => $user->name]) }}">
+                <li>
+                    <span>{{ $user->name }}</span>
+                    <span>{{ $user->articles_count }}件</span>
+                </li>
+            </a>
+        </div>
+    @endforeach
+</ul>
+
+<div class="ranking"><i class="fas fa-tags mr-2"></i>タグランキング</div>
+<ul class="user_ranking">
+    @foreach ($tags_ranking as $tag)
+        <div class="ranking-item">
+            <a href="{{ route('tags.show', ['name' => $tag->name]) }}">
+                <li>
+                    <span>{{ $tag->hashtag }}</span>
+                    <span>{{ $tag->articles_count }}件</span>
+                </li>
+            </a>
+        </div>
+    @endforeach
+</ul>
