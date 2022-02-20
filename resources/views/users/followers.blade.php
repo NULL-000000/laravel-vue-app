6dd649a94a6479ac7763b291162f274425f94b06
NULL-000000 <black.design.mini@gmail.com>
2022-02-21 08:34:24 +0900

    [fix]プロフィール画面の修正

--- resources/views/users/followers.blade.php
+++ resources/views/users/followers.blade.php
@@ -6,21 +6,22 @@
 
     @include('nav')
 
-    <div class="container">
-
-        @include('users.user')
-
-        <div class="tabs-item mt-3">
-            @include('users.tabs', ['hasArticles' => false, 'hasLikes' => false, 'hasFollowings' => false, 'hasFollowers' => true])
+    <main>
+        <div class="container">
+            @include('users.user')
+            <div class="tabs-item mt-3">
+                @include('users.tabs', ['hasArticles' => false, 'hasLikes' => false, 'hasFollowings' => false,
+                'hasFollowers' => true])
+            </div>
+            <div class="mb-5">
+                @foreach ($followers as $person)
+                    <div class="mt-3">
+                        @include('users.person')
+                    </div>
+                @endforeach
+            </div>
         </div>
-        <div class="mb-5">
-            @foreach ($followers as $person)
-                <div class="mt-3">
-                    @include('users.person')
-                </div>
-            @endforeach
-        </div>
-    </div>
+    </main>
 
     @include('footer')
 
