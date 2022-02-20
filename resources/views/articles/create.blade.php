8f79acfacda1758ff48b290017601346357ebc86
NULL-000000 <black.design.mini@gmail.com>
2022-02-21 08:36:33 +0900

    [fix]宣言新規投稿画面の修正

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -6,17 +6,19 @@
 
     @include('nav')
 
-    <div class="container mt-4">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="mb-4">
-                    <form method="POST" action="{{ route('articles.store') }}">
-                        @include('articles.form')
-                    </form>
+    <main>
+        <div class="container mt-3">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="mb-4">
+                        <form method="POST" action="{{ route('articles.store') }}">
+                            @include('articles.form')
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </main>
 
     @include('footer')
 
