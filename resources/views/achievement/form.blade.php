5b67880e77c22b90f5d7ba866cd1d9e55dba4e4c
NULL-000000 <black.design.mini@gmail.com>
2022-02-23 08:42:04 +0900

    [fix]達成チェック画面の修正

--- resources/views/achievement/form.blade.php
+++ resources/views/achievement/form.blade.php
@@ -14,10 +14,10 @@
                 @csrf
                 <div class="form-body">
                     @include('error_card_list')
-                    <label>学び・反省</label>
-                    <textarea name="study" required class="form-control mb-2" rows="2" placeholder="本文"></textarea>
+                    <label>反省・学び</label>
+                    <textarea name="study" required class="form-control my-2" rows="2" placeholder="反省"></textarea>
                     <label>次回の意気込み</label>
-                    <textarea name="enthusiasm" required class="form-control mb-2" rows="2" placeholder="本文"></textarea>
+                    <textarea name="enthusiasm" required class="form-control my-2" rows="2" placeholder="意気込み"></textarea>
                     <input type="hidden" name="achievement" value="success">
                     <input type="hidden" name="cause" value="unspecified">
                     <input type="hidden" name="solution" value="unspecified">
@@ -44,9 +44,9 @@
                 <div class="form-body">
                     @include('error_card_list')
                     <label>失敗した原因</label>
-                    <textarea name="cause" required class="form-control mb-2" rows="2" placeholder="本文"></textarea>
+                    <textarea name="cause" required class="form-control my-2" rows="2" placeholder="原因"></textarea>
                     <label>次回の対策</label>
-                    <textarea name="solution" required class="form-control mb-2" rows="2" placeholder="本文"></textarea>
+                    <textarea name="solution" required class="form-control my-2" rows="2" placeholder="対策"></textarea>
                     <input type="hidden" name="achievement" value="failure">
                     <input type="hidden" name="study" value="unspecified">
                     <input type="hidden" name="enthusiasm" value="unspecified">
